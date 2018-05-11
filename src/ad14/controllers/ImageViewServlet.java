810985cb0915d6611efc840d150b6fa901c79ee9
luan14t1 <taikhoantiphu.com>
2018-05-11 20:49:43 +0700

    AD-Luan

--- src/ad14/controllers/ImageViewServlet.java
+++ src/ad14/controllers/ImageViewServlet.java
@@ -17,14 +17,23 @@ import java.util.ArrayList;
 @WebServlet(name = "ImageViewServlet", urlPatterns ={WebURI.HINH_ANH})
 public class ImageViewServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//        Dao_HinhAnh HADAO = new Dao_HinhAnh(KetNoiCSDL.connect());
-//        ArrayList<HinhAnh> listItems = HADAO.getListHinhAnh();
-//        request.setAttribute("listItems", listItems);
-        RequestDispatcher rd = request.getRequestDispatcher("/image/index.jsp");
-        rd.forward(request, response);
+
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        doPost(request, response);
+        switch (request.getRequestURI()) {
+            case WebURI.HINH_ANH:
+                RequestDispatcher rd = request.getRequestDispatcher("/image/index.jsp");
+                rd.forward(request, response);
+                break;
+            case WebURI.HINH_ANH_THEM:
+                RequestDispatcher rd1 = request.getRequestDispatcher("/image/add.jsp");
+                rd1.forward(request, response);
+                break;
+            case WebURI.HINH_ANH_SUA:
+                RequestDispatcher rd2 = request.getRequestDispatcher("/image/edit.jsp");
+                rd2.forward(request, response);
+                break;
+        }
     }
 }
